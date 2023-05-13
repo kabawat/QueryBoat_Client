7f3a9cf2055fc50eec697bfdc73c0318a4d727a2
w3App <kshatriyakabawat@gmail.com>
2023-05-13 19:24:30 +0530

    Update scoket.js

--- src/redux/reducer/scoket.js
+++ src/redux/reducer/scoket.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client'
 
-// const url = `https://queryboat-api.onrender.com` 
-const url = 'http://localhost:2917'
+const url = `https://queryboat-api.onrender.com` 
+// const url = 'http://localhost:2917'
 // const url = 'https://query-boat-server.vercel.app'
 
 export const BaseUrl = (state = url, action) => {
